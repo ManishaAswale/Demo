175b943bd23eefbe4368a954819be222d212f1a7
Manisha <mpaswale@gmail.com>
2022-11-03 15:53:06 +0530

    new file added

--- demo.java
+++ demo.java
@@ -1,5 +1,16 @@
+<<<<<<< Updated upstream
 public void show()
 {
 sop("welcome to infosys ");
+=======
+public static void main(String args[])
+{
+int a,b,c;
+a=10;
+b=20;
+c=b-a;
+sop("subtraction is = ");
+
+>>>>>>> Stashed changes
 
 }
